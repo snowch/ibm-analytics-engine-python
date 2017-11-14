7ccb8a8910fbe5f46de19cf6da7f50438f8a26d7
christopher snow <chsnow123@gmail.com>
2017-11-14 12:07:32 +0000

    fix pypi meta

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
   name='ibm-analytics-engine-python',
-  version='0.0.7',
+  version='0.0.8',
   description="IBM Analytics Engine library",
   author='Chris Snow',
   author_email='chsnow123@gmail.com',
   url='https://github.com/snowch/ibm-analytics-engine-python',
-  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.7.tar.gz',
+  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.8.tar.gz',
   packages = find_packages(exclude=["docs/example", "tests"]),
-  keywords = 'IBM Analytics Engine, Spark, Hadoop',
+  keywords = 'IBM Analytics Engine Spark Hadoop',
   install_requires=[ 'requests' ],
   test_suite='nose.collector',
   tests_require=['nose'],
