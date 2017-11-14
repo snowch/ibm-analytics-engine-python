cb0e32f339617aba978d77027d889f109cbb6d4e
christopher snow <chsnow123@gmail.com>
2017-11-14 11:29:50 +0000

    add keyworks and description

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup, find_packages
 setup(
   name='ibm-analytics-engine-python',
   version='0.0.1',
+  descripton="IBM Analytics Engine library",
   author='Chris Snow',
   author_email='chsnow123@gmail.com',
   url='https://github.com/snowch/ibm-analytics-engine-python',
+  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.1.tar.gz',
   packages = find_packages(exclude=["docs/example", "tests"]),
+  keywords = ['IBM', 'analytics', 'engine', 'spark', 'hadoop'],
   install_requires=[ 'requests' ],
   test_suite='nose.collector',
   tests_require=['nose'],
