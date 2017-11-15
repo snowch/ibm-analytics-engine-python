7b89e2a1d203734d30c4457c4599997b12a5845c
christopher snow <chsnow123@gmail.com>
2017-11-15 19:34:31 +0000

    add python versions

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
   name='ibm-analytics-engine-python',
-  version='0.0.8',
+  version='0.0.9',
   description="IBM Analytics Engine library",
   author='Chris Snow',
   author_email='chsnow123@gmail.com',
   url='https://github.com/snowch/ibm-analytics-engine-python',
-  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.8.tar.gz',
+  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.9.tar.gz',
   packages = find_packages(exclude=["docs/example", "tests"]),
   keywords = 'IBM Analytics Engine Spark Hadoop',
   install_requires=[ 'requests' ],
@@ -17,5 +17,11 @@ setup(
   classifiers=[
     "Development Status :: 3 - Alpha",
     "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
   ],
 )
