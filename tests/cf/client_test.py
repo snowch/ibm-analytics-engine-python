9ffabc5e84c428b0b88a96fed7d62381aae3a637
christopher snow <chsnow123@gmail.com>
2017-11-15 20:16:44 +0000

    add cf client test

--- tests/cf/client_test.py
+++ tests/cf/client_test.py
@@ -1,14 +1,34 @@
 from unittest import TestCase
-
+import sys
+import tempfile
+import os
+import json
 
 from ibm_analytics_engine import CloudFoundryAPI
 
+
 class TestCloudFoundryAPI(TestCase):
-    def test_api_key_file_is_accessed(self):
+    def test_invalid_api_key_file(self):
         try:
             error_class = IOError
-        except:
+        except BaseException:
             error_class = FileNotFoundError
 
         with self.assertRaises(error_class):
             cf = CloudFoundryAPI(api_key_filename='does_not_exist')
+
+    def test_api_key_file(self):
+        # delete=True means the file will be deleted on close
+        tmp = tempfile.NamedTemporaryFile(delete=True)
+        try:
+            data = json.dumps({
+                "name": "iae-key",
+                "description": "",
+                "createdAt": "2017-11-14T12:30+0000",
+                             "apiKey": ""
+            })
+            tmp.write(data)
+            tmp.flush()
+            cf = CloudFoundryAPI(api_key_filename=tmp.name)
+        finally:
+            tmp.close()  # deletes the file
