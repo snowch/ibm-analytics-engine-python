52e8a263ba5424cc4303388aebd1e5008d6d0f59
christopher snow <chsnow123@gmail.com>
2017-11-17 16:01:16 +0000

    move doc script to file

---
+++ docs/example_scripts/create_cluster.py
@@ -0,0 +1,29 @@
+from ibm_analytics_engine import CloudFoundryAPI, IAE, IAEServicePlanGuid
+
+os.environ["LOG_LEVEL"] = "DEBUG"
+
+cf_api_key_filename = sys.argv[1]
+new_cluster_name = sys.argv[2]
+space_guid = sys.argv[3]
+
+cf = CloudFoundryAPI(api_key_filename=cf_api_key_filename)
+iae = IAE(cf_client=cf)
+
+cluster_instance_id = iae.create_cluster(
+    service_instance_name=new_cluster_name,
+    service_plan_guid=IAEServicePlanGuid.LITE,
+    space_guid=space_guid,
+    customization_script={
+        "hardware_config": "default",
+        "num_compute_nodes": 1,
+        "software_package": "ae-1.0-spark",
+    }
+)
+print('>> IAE cluster instance id: {}'.format(cluster_instance_id))
+status = iae.get_cluster_status(cluster_instance_id)
+if status == 'succeeded':
+   credentials_json = iae.create_credentials(cluster_instance_id)
+   vcap_json = credentials_json['entity']['credentials']
+   print('>> VCAP:\n' + json.dumps(vcap_json, indent=4, separators=(',', ': ')))
+else:
+   print('>> Cluster status: {}'.format(status))
