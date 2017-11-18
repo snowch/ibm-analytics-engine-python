e65661f05820773763654b8c478a47181826939e
christopher snow <chsnow123@gmail.com>
2017-11-18 07:21:16 +0000

    bump version to 0.0.11

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
   name='ibm-analytics-engine-python',
-  version='0.0.10',
+  version='0.0.11',
   description="IBM Analytics Engine library",
   author='Chris Snow',
   author_email='chsnow123@gmail.com',
   url='https://github.com/snowch/ibm-analytics-engine-python',
-  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.10.tar.gz',
+  download_url = 'https://github.com/snowch/ibm-analytics-engine-python/archive/0.0.11.tar.gz',
   packages = find_packages(exclude=["docs/example_scripts", "docs/example", "tests"]),
   keywords = 'IBM Analytics Engine Spark Hadoop',
   install_requires=[ 'requests' ],
