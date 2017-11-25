84d0ef5b1b9399b67ce74a5b474aeae5bec63ece
christopher snow <chsnow123@gmail.com>
2017-11-25 17:26:51 +0000

    show polling for status

--- docs/example_scripts/cluster_status.py
+++ docs/example_scripts/cluster_status.py
@@ -1,4 +1,5 @@
 import os
+import time
 from ibm_analytics_engine import CloudFoundryAPI, IAE
 
 os.environ["LOG_LEVEL"] = "DEBUG"
@@ -8,6 +9,10 @@ cluster_instance_guid = os.environ['CLUSTER_INSTANCE_GUID']
 
 cf = CloudFoundryAPI(api_key_filename=cf_api_key_filename)
 iae = IAE(cf_client=cf)
-status = iae.status(cluster_instance_guid)
+
+while True:
+    status = iae.status(cluster_instance_id=cluster_instance_guid)
+    if status == 'succeeded' or status == 'failed': break
+    time.sleep(60)
 
 print(status)
