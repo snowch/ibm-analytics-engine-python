2be334d7fd642b08ab97d42c149122cf55ba6b3d
christopher snow <chsnow123@gmail.com>
2017-11-29 10:58:59 +0000

    simplify status with poll example

--- docs/example_scripts/cluster_status_poll.py
+++ docs/example_scripts/cluster_status_poll.py
@@ -1,15 +1,11 @@
-import os
-from ibm_analytics_engine import CloudFoundryAPI, IAE
+from ibm_analytics_engine.cf.client import CloudFoundryAPI
+from ibm_analytics_engine import IAE
 
-os.environ["LOG_LEVEL"] = "DEBUG"
-
-cf_api_key_filename = os.environ['API_KEY_FILENAME']
-cluster_instance_guid = os.environ['CLUSTER_INSTANCE_GUID']
-
-cf = CloudFoundryAPI(api_key_filename=cf_api_key_filename)
+cf = CloudFoundryAPI(api_key_filename='your_api_key_filename')
 iae = IAE(cf_client=cf)
+
 status = iae.status(
-    cluster_instance_guid=cluster_instance_guid,
+    cluster_instance_guid='12345-12345-12345-12345',
     poll_while_in_progress=True)
 
 print(status)
--- tests/doc/test_cluster_status_poll.py
+++ tests/doc/test_cluster_status_poll.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import tempfile
 import json
@@ -24,23 +25,14 @@ class DocExampleScripts_Test(TestCase):
 
         if args[0] == 'https://api.ng.bluemix.net/v2/info':
             return MockResponse({"authorization_endpoint": "https://login.ng.bluemix.net/UAALoginServerWAR"}, 200)
-        elif args[0] == 'https://api.ng.bluemix.net/v2/service_instances/1234567890':
-            return MockResponse(
-                    {"metadata": {"guid": "1234567890", "url": "/v2/service_instances/1234567890", "created_at": "2017-11-16T15:23:26Z", "updated_at": "2017-11-16T15:23:26Z"}, "entity": {"name": "AE", "credentials": {}, "service_plan_guid": "acb06a56-fab1-4cb1-a178-c811bc676164", "space_guid": "1234567890", "gateway_data": None, "dashboard_url": "https://ibmae-ui.mybluemix.net/analytics/engines/paygo/jumpout?apiKey=1234567890", "type": "managed_service_instance", "last_operation": {"type": "create", "state": "succeeded", "description": "", "updated_at": "2017-11-16T15:23:26Z", "created_at": "2017-11-16T15:23:26Z"}, "tags": [], "service_guid": "1234567890", "space_url": "/v2/spaces/1234567890", "service_plan_url": "/v2/service_plans/1234567890", "service_bindings_url": "/v2/service_instances/1234567890/service_bindings", "service_keys_url": "/v2/service_instances/1234567890/service_keys", "routes_url": "/v2/service_instances/1234567890/routes", "service_url": "/v2/services/1234567890"}}, 200)
-
+        elif args[0] == 'https://api.ng.bluemix.net/v2/service_instances/12345-12345-12345-12345':
+            return MockResponse({"metadata": {"guid": "1234567890"}, "entity": {"name": "AE", "last_operation": {"type": "create", "state": "succeeded"}}}, 200)
         raise RuntimeError("Mock response not found for GET: {}".format(args[0]))
 
     def mocked_requests_post(*args, **kwargs):
 
         if args[0] == 'https://login.ng.bluemix.net/UAALoginServerWAR/oauth/token':
             return MockResponse({'access_token': 'aaaaa', 'token_type': 'bearer', 'refresh_token': 'aaaaa', 'expires_in': 1209599}, 200)
-#        elif args[0] == 'https://api.ng.bluemix.net/v2/service_instances?accepts_incomplete=true':
-#            return MockResponse(
-#                    {"metadata": {"guid": "1234567890", "url": "/v2/service_instances/1234567890", "created_at": "2017-11-16T15:23:26Z", "updated_at": "2017-11-16T15:23:26Z"}, "entity": {"name": "AE", "credentials": {}, "service_plan_guid": "acb06a56-fab1-4cb1-a178-c811bc676164", "space_guid": "1234567890", "gateway_data": None, "dashboard_url": "https://ibmae-ui.mybluemix.net/analytics/engines/paygo/jumpout?apiKey=1234567890", "type": "managed_service_instance", "last_operation": {"type": "create", "state": "in progress", "description": "", "updated_at": "2017-11-16T15:23:26Z", "created_at": "2017-11-16T15:23:26Z"}, "tags": [], "service_guid": "1234567890", "space_url": "/v2/spaces/1234567890", "service_plan_url": "/v2/service_plans/1234567890", "service_bindings_url": "/v2/service_instances/1234567890/service_bindings", "service_keys_url": "/v2/service_instances/1234567890/service_keys", "routes_url": "/v2/service_instances/1234567890/routes", "service_url": "/v2/services/1234567890"}}, 200)
-#        elif args[0] == 'https://api.ng.bluemix.net/v2/service_keys':
-#            return MockResponse(
-#                    {"metadata": {"guid": "1234567890", "url": "/v2/service_keys/1234567890", "created_at": "2017-11-16T15:33:52Z", "updated_at": "2017-11-16T15:33:52Z"}, "entity": {"name": "Credentials-1", "service_instance_guid": "1234567890", "credentials": {"cluster_management": {"api_url": "https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890", "instance_id": "1234567890"}, "cluster": {"cluster_id": "1234567890", "user": "clsadmin", "password": "XXXXXXXXXX", "service_endpoints": {"ambari_console": "https://testhostname:9443", "livy": "https://testhostname:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://testhostname:8443/gateway/default/sparkhistory", "notebook_gateway_websocket": "wss://testhostname:8443/gateway/default/jkgws/", "notebook_gateway": "https://testhostname:8443/gateway/default/jkg/", "webhdfs": "https://testhostname:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@testhostname"}, "service_endpoints_ip": {"ambari_console": "https://1.1.1.1:9443", "livy": "https://1.1.1.1:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://1.1.1.1:8443/gateway/default/sparkhistory", "notebook_gateway_websocket": "wss://1.1.1.1:8443/gateway/default/jkgws/", "notebook_gateway": "https://1.1.1.1:8443/gateway/default/jkg/", "webhdfs": "https://1.1.1.1:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@1.1.1.1"}}}, "service_instance_url": "/v2/service_instances/1234567890"}}, 200)
-#
         raise RuntimeError("Mock response not found for POST: {}".format(args[0]))
 
     @mock.patch('requests.get', side_effect=mocked_requests_get)
@@ -58,15 +50,13 @@ class DocExampleScripts_Test(TestCase):
             tmp.write(data)
             tmp.flush()
         
-            os.environ['API_KEY_FILENAME'] = tmp.name
-            os.environ['CLUSTER_INSTANCE_GUID'] = '1234567890'
+            from ibm_analytics_engine import CloudFoundryAPI
+            CloudFoundryAPI.api_key_filename = tmp.name
 
-            scriptfile = os.path.abspath(os.path.join(scriptDir, 'cluster_status_poll.py'))
-            try:
-                # Python 2x
-                execfile(scriptfile)
-            except:
-                # Python 3x
-                exec(open(scriptfile).read())
+            sys.path.append(os.path.abspath(os.path.join(scriptDir)))
+            import cluster_status_poll
+
         finally:
             tmp.close()  # deletes the file
+            if hasattr(CloudFoundryAPI, 'api_key_filename'):
+                del CloudFoundryAPI.api_key_filename
