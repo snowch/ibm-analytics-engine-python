75ba5cd111c025ed24f7a5643a063239c752ea19
christopher snow <chsnow123@gmail.com>
2017-11-29 11:08:33 +0000

    simplify DP status example

--- docs/example_scripts/cluster_dp_api_status.py
+++ docs/example_scripts/cluster_dp_api_status.py
@@ -1,15 +1,11 @@
-import os
-from ibm_analytics_engine import CloudFoundryAPI, IAE
+from ibm_analytics_engine.cf.client import CloudFoundryAPI
+from ibm_analytics_engine import IAE
 
-os.environ["LOG_LEVEL"] = "DEBUG"
+cf = CloudFoundryAPI(api_key_filename='your_api_key_filename')
 
-cf_api_key_filename = os.environ['API_KEY_FILENAME']
-cluster_instance_guid = os.environ['CLUSTER_INSTANCE_GUID']
-
-cf = CloudFoundryAPI(api_key_filename=cf_api_key_filename)
 iae = IAE(cf_client=cf)
 
-vcap = iae.get_or_create_credentials(cluster_instance_guid)
+vcap = iae.get_or_create_credentials(cluster_instance_guid='12345-12345-12345-12345')
 
 status = iae.dataplatform_status(vcap)
 
--- tests/doc/test_cluster_dp_api_status.py
+++ tests/doc/test_cluster_dp_api_status.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import tempfile
 import json
@@ -25,12 +26,9 @@ class DocExampleScripts_Test(TestCase):
 
         if args[0] == 'https://api.ng.bluemix.net/v2/info':
             return MockResponse({"authorization_endpoint": "https://login.ng.bluemix.net/UAALoginServerWAR"}, 200)
-        if args[0] == 'https://api.ng.bluemix.net/v2/spaces/1234567890/summary':
+        if args[0] == 'https://api.ng.bluemix.net/v2/service_keys?q=service_instance_guid:12345-12345-12345-12345':
             return MockResponse(
-                {"guid": "1234567890", "name": "1234567890", "apps": [], "services": [{"guid": "1234567890", "name": "MYIAE", "bound_app_count": 0, "last_operation": {"type": "create", "state": "succeeded", "description": "", "updated_at": "2017-11-17T22:47:00Z", "created_at": "2017-11-17T22:25:52Z"}, "dashboard_url": "https://ibmae-ui.mybluemix.net/analytics/engines/paygo/jumpout?apiKey=1234567890&instanceId=1234567890", "service_plan": {"guid": "1234567890", "name": "lite", "service": {"guid": "1234567890", "label": "IBMAnalyticsEngine", "provider": None, "version": None}}}]}, 200)
-        if args[0] == 'https://api.ng.bluemix.net/v2/service_keys?q=service_instance_guid:1234567890':
-            return MockResponse(
-                {"total_results": 1, "total_pages": 1, "prev_url": None, "next_url": None, "resources": [{"metadata": {"guid": "1234567890", "url": "/v2/service_keys/1234567890", "created_at": "2017-11-17T22:48:03Z", "updated_at": "2017-11-17T22:48:03Z"}, "entity": {"name": "Credentials-1", "service_instance_guid": "1234567890", "credentials": {"cluster_management": {"api_url": "https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890", "instance_id": "1234567890"}, "cluster": {"cluster_id": "20171117-222539-855-AibSBgvm", "user": "clsadmin", "password": "XXXXXXX", "service_endpoints": {"ambari_console": "https://testhostname:9443", "livy": "https://testhostname:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://testhostname:8443/gateway/default/sparkhistory", "oozie_rest": "https://testhostname:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://testhostname:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://testhostname:8443/gateway/default/jkgws/", "notebook_gateway": "https://testhostname:8443/gateway/default/jkg/", "webhdfs": "https://testhostname:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@testhostname", "spark_sql": "jdbc:hive2://testhostname:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}, "service_endpoints_ip": {"ambari_console": "https://1.1.1.1:9443", "livy": "https://1.1.1.1:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://1.1.1.1:8443/gateway/default/sparkhistory", "oozie_rest": "https://1.1.1.1:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://1.1.1.1:8443/gateway/default/jkgws/", "notebook_gateway": "https://1.1.1.1:8443/gateway/default/jkg/", "webhdfs": "https://1.1.1.1:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@1.1.1.1", "spark_sql": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}}}, "service_instance_url": "/v2/service_instances/1234567890"}}]}, 200)
+                {"total_results": 1, "resources": [{"metadata": {"guid": "1234567890"}, "entity": {"name": "Credentials-1", "service_instance_guid": "1234567890", "credentials": {"cluster_management": {"api_url": "https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890", "instance_id": "1234567890"}, "cluster": {"cluster_id": "20171117-222539-855-AibSBgvm", "user": "clsadmin", "password": "XXXXXXX", "service_endpoints": {"ambari_console": "https://testhostname:9443", "livy": "https://testhostname:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://testhostname:8443/gateway/default/sparkhistory", "oozie_rest": "https://testhostname:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://testhostname:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://testhostname:8443/gateway/default/jkgws/", "notebook_gateway": "https://testhostname:8443/gateway/default/jkg/", "webhdfs": "https://testhostname:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@testhostname", "spark_sql": "jdbc:hive2://testhostname:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}, "service_endpoints_ip": {"ambari_console": "https://1.1.1.1:9443", "livy": "https://1.1.1.1:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://1.1.1.1:8443/gateway/default/sparkhistory", "oozie_rest": "https://1.1.1.1:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://1.1.1.1:8443/gateway/default/jkgws/", "notebook_gateway": "https://1.1.1.1:8443/gateway/default/jkg/", "webhdfs": "https://1.1.1.1:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@1.1.1.1", "spark_sql": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}}}}}]}, 200)
         if args[0] == 'https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890/state':
             return MockResponse({'state': 'Active'}, 200)
 
@@ -41,9 +39,9 @@ class DocExampleScripts_Test(TestCase):
             return MockResponse({'access_token': 'aaaaa', 'token_type': 'bearer', 'refresh_token': 'aaaaa', 'expires_in': 1209599}, 200)
         if args[0] == 'https://iam.bluemix.net/identity/token':
             return MockResponse({'access_token': 'aaaaa', 'token_type': 'bearer', 'refresh_token': 'aaaaa', 'expires_in': 1209599}, 200)
-        if args[0] == 'https://api.ng.bluemix.net/v2/service_keys?q=service_instance_guid:1234567890':
+        if args[0] == 'https://api.ng.bluemix.net/v2/service_keys?q=service_instance_guid:12345-12345-12345-12345':
             return MockResponse(
-                {"metadata": {"guid": "1234567890", "url": "/v2/service_keys/1234567890", "created_at": "2017-11-17T22:48:03Z", "updated_at": "2017-11-17T22:48:03Z"}, "entity": {"name": "Credentials-1", "service_instance_guid": "1234567890", "credentials": {"cluster_management": {"api_url": "https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890", "instance_id": "1234567890"}, "cluster": {"cluster_id": "1234567890", "user": "clsadmin", "password": "XXXXXXXX", "service_endpoints": {"ambari_console": "https://testhost:9443", "livy": "https://testhost:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://testhost:8443/gateway/default/sparkhistory", "oozie_rest": "https://testhost:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://testhost:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://testhost:8443/gateway/default/jkgws/", "notebook_gateway": "https://testhost:8443/gateway/default/jkg/", "webhdfs": "https://testhost:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@testhost", "spark_sql": "jdbc:hive2://testhost:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}, "service_endpoints_ip": {"ambari_console": "https://1.1.1.1:9443", "livy": "https://1.1.1.1:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://1.1.1.1:8443/gateway/default/sparkhistory", "oozie_rest": "https://1.1.1.1:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://1.1.1.1:8443/gateway/default/jkgws/", "notebook_gateway": "https://1.1.1.1:8443/gateway/default/jkg/", "webhdfs": "https://1.1.1.1:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@1.1.1.1", "spark_sql": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}}}, "service_instance_url": "/v2/service_instances/1234567890"}}, 200)
+                {"metadata": {"guid": "1234567890"}, "entity": {"name": "Credentials-1", "service_instance_guid": "1234567890", "credentials": {"cluster_management": {"api_url": "https://api.dataplatform.ibm.com/v2/analytics_engines/1234567890", "instance_id": "1234567890"}, "cluster": {"cluster_id": "1234567890", "user": "clsadmin", "password": "XXXXXXXX", "service_endpoints": {"ambari_console": "https://testhost:9443", "livy": "https://testhost:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://testhost:8443/gateway/default/sparkhistory", "oozie_rest": "https://testhost:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://testhost:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://testhost:8443/gateway/default/jkgws/", "notebook_gateway": "https://testhost:8443/gateway/default/jkg/", "webhdfs": "https://testhost:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@testhost", "spark_sql": "jdbc:hive2://testhost:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}, "service_endpoints_ip": {"ambari_console": "https://1.1.1.1:9443", "livy": "https://1.1.1.1:8443/gateway/default/livy/v1/batches", "spark_history_server": "https://1.1.1.1:8443/gateway/default/sparkhistory", "oozie_rest": "https://1.1.1.1:8443/gateway/default/oozie", "hive_jdbc": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/hive", "notebook_gateway_websocket": "wss://1.1.1.1:8443/gateway/default/jkgws/", "notebook_gateway": "https://1.1.1.1:8443/gateway/default/jkg/", "webhdfs": "https://1.1.1.1:8443/gateway/default/webhdfs/v1/", "ssh": "ssh clsadmin@1.1.1.1", "spark_sql": "jdbc:hive2://1.1.1.1:8443/;ssl=true;transportMode=http;httpPath=gateway/default/spark"}}}}}, 200)
 
         raise RuntimeError("Unhandle POST request: " + args[0]) 
 
@@ -62,16 +60,13 @@ class DocExampleScripts_Test(TestCase):
             tmp.write(data)
             tmp.flush()
         
-            os.environ['API_KEY_FILENAME'] = tmp.name
-            os.environ['CLUSTER_INSTANCE_GUID'] = '1234567890'
+            from ibm_analytics_engine import CloudFoundryAPI
+            CloudFoundryAPI.api_key_filename = tmp.name
 
-            scriptfile = os.path.abspath(os.path.join(scriptDir, 'cluster_dp_api_status.py'))
-            try:
-                # Python 2x
-                execfile(scriptfile)
-            except:
-                # Python 3x
-                exec(open(scriptfile).read())
+            sys.path.append(os.path.abspath(os.path.join(scriptDir)))
+            import cluster_dp_api_status 
+
         finally:
             tmp.close()  # deletes the file
-
+            if hasattr(CloudFoundryAPI, 'api_key_filename'):
+                del CloudFoundryAPI.api_key_filename
